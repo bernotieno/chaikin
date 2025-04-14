7b96b981cfaaccd1dc531066d5663cbf91a69ae5
Bshisia <shisiabrian7@gmail.com>
2025-04-14 10:02:12 +0300

    chore: add all missiong modules

--- chaikin/src/main.rs
+++ chaikin/src/main.rs
@@ -1,8 +1,10 @@
-use ui;
+mod model;
+mod window;
+mod ui;
+mod chaikin;
 
 fn main() {
-    nannou::app(model)
-        .update(update)
+    nannou::app(window::model)
+        .update(window::update)
         .run();
 }
-
